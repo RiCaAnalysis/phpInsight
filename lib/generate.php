51cc12cd565adc6fe2db08343ef7fe458d814205
Dima Udod <udod.dmitry@gmail.com>
2019-09-23 17:13:29 +0300

    Fix dictionary generation

--- lib/generate.php
+++ lib/generate.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once __DIR__ . './../vendor/autoload.php';
+require_once "PHPInsight/Sentiment.php";
+require_once "PHPInsight/Autoloader.php";
 
 echo "Generate dictionaries\n";
 (new PHPInsight\Sentiment())->reloadDictionaries();
